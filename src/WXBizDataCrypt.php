94cf4141c401f90a2164c0801f7453f2bda10d10
cyd622 <luffywang622@gmail.com>
2019-04-23 17:57:57 +0800

    fix bug

--- src/WXBizDataCrypt.php
+++ src/WXBizDataCrypt.php
@@ -30,7 +30,7 @@ class WXBizDataCrypt
      * 检验数据的真实性，并且获取解密后的明文.
      * @param string $encryptedData 加密数据
      * @param string $iv 加密算法的初始向量
-     * @return string 解密得到的明文
+     * @return array $data 解密得到的明文
      * @throws WeChatOAuthException
      */
     public function decrypt($encryptedData, $iv)
@@ -47,20 +47,7 @@ class WXBizDataCrypt
         $aesIV = base64_decode($iv);
         $aesCipher = base64_decode($encryptedData);
 
-        try {
-            $module = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
-            mcrypt_generic_init($module, $aesKey, $aesIV);
-            $decrypted = mdecrypt_generic($module, $aesCipher);
-            mcrypt_generic_deinit($module);
-            mcrypt_module_close($module);
-        } catch (\Exception $e) {
-            throw new WeChatOAuthException(ErrorCode::ILLEGAL_AES_KEY);
-        }
-        try {
-            $result = $this->removeComplementText($decrypted);
-        } catch (\Exception $e) {
-            throw new WeChatOAuthException(ErrorCode::ILLEGAL_BUFFER);
-        }
+        $result=openssl_decrypt( $aesCipher, "AES-128-CBC", $aesKey, 1, $aesIV);
 
         $data = json_decode($result, true);
         if ($data == NULL) {
@@ -70,19 +57,5 @@ class WXBizDataCrypt
             throw new WeChatOAuthException(ErrorCode::ILLEGAL_UN_BELONG);
         }
         return $data;
-    }
-
-    /**
-     * 去除补位字符
-     * @param $text
-     * @return bool|string
-     */
-    private function removeComplementText($text)
-    {
-        $pad = ord(substr($text, -1));
-        if ($pad < 1 || $pad > 32) {
-            $pad = 0;
-        }
-        return substr($text, 0, (strlen($text) - $pad));
     }
 }
